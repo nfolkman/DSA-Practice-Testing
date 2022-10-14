4e620bb91b16c062baceb194d3ee936251e482f5
nfolkman <nathanielfolkman@gmail.com>
2022-10-13 19:28:27 -0700

    vowelsCounter algorithms

--- Beginner/vowelsCounter/index-START.js
+++ Beginner/vowelsCounter/index-START.js
@@ -3,10 +3,26 @@ Given a string of text, return the number of vowels found within the text
 e.g vowelsCounter('anehizxcv') // will return 3
 */
 
-
+    // for-loop solution  O(N)  (80% FASTER)
 function vowelsCounter(text) {
-    // Code goes here
+    let count = 0
+
+    for(let char of text){
+        if(char.toLowerCase() === 'a' || char.toLowerCase() === 'e' || char.toLowerCase() === 'i' || char.toLowerCase() === 'o' || char.toLowerCase() === 'u') count++
+    }
+    return count
 }
+
+
+    // regex solution
+    function vowelsCounter(text){
+        // set matching instances
+        let matchingInstances = text.match(/[aeiou]/gi)
+
+        // check if matching instances exist
+        if(matchingInstances) return matchingInstances.length
+        else return 0
+    }
 
 
 
